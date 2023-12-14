6d5d12b489c4d6cbac4ecc70e3bb465bac247b78
Leonard M Reidy <leonard.reidy@gmail.com>
2023-12-14 05:49:05 +0000

    Restore to baseline

--- src/app/gilded-rose.ts
+++ src/app/gilded-rose.ts
@@ -1,30 +1,69 @@
-import { Item } from "./components/item.component";
-import { InventoryRulesEngine } from "./components/rules.engine.component";
-import { CoreResultType, CoreRuleType } from "./core/core.rules.engine";
-import { InventoryService } from "./services/inventory/inventory.service";
+export class Item {
+  name: string;
+  sellIn: number;
+  quality: number;
 
-export class GildedRose<Rule extends CoreRuleType, Result extends CoreResultType> {
-  private _items: Array<Item>;
-  private _inventoryService: InventoryService<Rule, Result>;
+  constructor(name, sellIn, quality) {
+    this.name = name;
+    this.sellIn = sellIn;
+    this.quality = quality;
+  }
+}
 
-  constructor(
-    items: Array<Item>,
-    inventoryService?: InventoryService<Rule, Result>,
-  ) {
-    this._items = items;
-    this._inventoryService = inventoryService ??
-      new InventoryService<Rule, Result>(new InventoryRulesEngine<Rule, Result>());
+export class GildedRose {
+  items: Array<Item>;
+
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
   }
 
-  get items(): Array<Item> {
-    return this._items;
-  }
+  updateQuality() {
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+        if (this.items[i].quality > 0) {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+        }
+      } else {
+        if (this.items[i].quality < 50) {
+          this.items[i].quality = this.items[i].quality + 1
+          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].sellIn < 11) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+            if (this.items[i].sellIn < 6) {
+              if (this.items[i].quality < 50) {
+                this.items[i].quality = this.items[i].quality + 1
+              }
+            }
+          }
+        }
+      }
+      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+      }
+      if (this.items[i].sellIn < 0) {
+        if (this.items[i].name != 'Aged Brie') {
+          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
+            if (this.items[i].quality > 0) {
+              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+                this.items[i].quality = this.items[i].quality - 1
+              }
+            }
+          } else {
+            this.items[i].quality = this.items[i].quality - this.items[i].quality
+          }
+        } else {
+          if (this.items[i].quality < 50) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+        }
+      }
+    }
 
-  get inventoryService(): InventoryService<Rule, Result> {
-    return this._inventoryService;
-  }
-
-  public updateQuality() {
-    return this._inventoryService.updateQuality(this.items);
+    return this.items;
   }
 }
